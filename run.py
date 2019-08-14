2c67f885c4f5294e006abacec8285839de4be922
misha <easydriverup@gmail.com>
2019-08-13 23:19:33 -0500

    changed run.py

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ from app.models import User, Product
 @app.shell_context_processor
 def make_shell_context():
   return {'db': db, 'User': User, 'Product': Product}
+
+if __name__ == '__main__':
+    app.run()
